294131cd2d0520914cb64ccb486af74d0ae5d2ae
muhammetbilgin <muhammet.bilgin@mobven.com>
2024-03-13 23:39:25 +0300

    feature: mobile pages organized

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const products = [
   { id: 1, name: '365 Signature Hoodie', price: 33.95, currency: "€", image:"./assets/images/product-image/product-1.png", color: "green" },
-  { id: 2, name: 'Organic Skinny High Waist Jeans', price: 33.95, currency: "€", image:"./assets/images/product-image/product-2.png", color: "" },
-  { id: 3, name: 'Organic Skinny High Waist Jeans', price: 33.95, currency: "€", image:"./assets/images/product-image/product-3.png" },
+  { id: 2, name: 'Organic Skinny High Waist Jeans', price: 33.95, currency: "€", image:"./assets/images/product-image/product-2.png", color: "cream" },
+  { id: 3, name: 'Organic Skinny High Waist Jeans', price: 33.95, currency: "€", image:"./assets/images/product-image/product-3.png", color: "blue" },
 ];
 
 // Ürünleri HTML listesine dönüştürme
@@ -11,16 +11,16 @@ function renderProductList(products) {
       const div = document.createElement('div');
       div.classList.add('product-container')
       div.innerHTML = `
-        <img src="${product.image}" alt='product-image' />
-        <span class='product-name'>${product.name}</span>
-        <div class='product-price-container'>
+        <img draggable='false' src='${product.image}' alt='product-image' />
+        <span draggable='false' class='product-name'>${product.name}</span>
+        <div draggable='false' class='product-price-container'>
           <span class='product-price'> ${product.currency}${product.price} </span>
           <span class='product-color'>
             <ul>
-              <li class=''></li>
-              <li class=''></li>
-              <li class=''></li>
-              <li class=''></li>
+              <li class='red'></li>
+              <li class='green'></li>
+              <li class='cream'></li>
+              <li class='blue'></li>
             </ul>
           </span>
         </div>
