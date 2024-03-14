9533fbf9cb655190152fb0516ca63deccfcd992b
muhammetbilgin <muhammet.bilgin@mobven.com>
2024-03-15 01:33:07 +0300

    bugfix: products color and mobile desing edited

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const products = [
-  { id: 1, name: '365 Signature Hoodie', price: 33.95, currency: "€", image:"./assets/images/product-image/product-1.png", color: "green" },
-  { id: 2, name: 'Organic Skinny High Waist Jeans', price: 33.95, currency: "€", image:"./assets/images/product-image/product-2.png", color: "cream" },
-  { id: 3, name: 'Organic Skinny High Waist Jeans', price: 33.95, currency: "€", image:"./assets/images/product-image/product-3.png", color: "blue" },
+  { id: 1, name: '365 Signature Hoodie', mobileName: "Organic Skinny High Waist Raw Hem Jeans", price: 33.95, currency: "€", image:"./assets/images/product-image/product-1.png", color: "green" },
+  { id: 2, name: 'Organic Skinny High Waist Jeans', mobileName: "Organic Skinny High Waist Raw Hem Jeans", price: 33.95, currency: "€", image:"./assets/images/product-image/product-2.png", color: "cream" },
+  { id: 3, name: 'Organic Skinny High Waist Jeans', mobileName: "Organic Skinny High Waist Raw Hem Jeans", price: 33.95, currency: "€", image:"./assets/images/product-image/product-3.png", color: "blue" },
 ];
 
-// Ürünleri HTML listesine dönüştürme
+const mobileMediaQuery = 800;
+
+function handleScreenSizeChange(products, index) {
+  if (window.innerWidth < mobileMediaQuery) {
+    console.log("gir", products);
+    return products[index].mobileName;
+  } else {
+    return products[index].name;
+  }
+}
+
 function renderProductList(products) {
   const productListDiv = document.getElementById('productList');
-  products.forEach(product => {
+  products.forEach((product, index) => {
       const div = document.createElement('div');
       div.classList.add('product-container')
       div.innerHTML = `
-        <img draggable='false' src='${product.image}' alt='product-image' />
-        <span draggable='false' class='product-name'>${product.name}</span>
+        <img draggable='false' src='${product.image}' alt='product-image'/>
+        <div draggable='false' class='product-name'>${handleScreenSizeChange(products, index)}</div>
         <div draggable='false' class='product-price-container'>
           <span class='product-price'> ${product.currency}${product.price} </span>
           <span class='product-color'>
             <ul>
-              <li class='red'></li>
+              <li class='blue selected'></li>
+              <li class='brown'></li>
+              <li class='pink'></li>
               <li class='green'></li>
-              <li class='cream'></li>
-              <li class='blue'></li>
             </ul>
           </span>
         </div>
@@ -29,7 +39,7 @@ function renderProductList(products) {
   });
 }
 
-// Sayfa yüklendiğinde ürün listesini oluştur
+
 document.addEventListener('DOMContentLoaded', () => {
   renderProductList(products);
 });
